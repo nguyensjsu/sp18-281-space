992954d04afca8835449f2f8f3297a9d30e78e7a
hoang1127 <hoang1127@gmail.com>
2018-05-04 01:46:44 -0700

    Set connection Redis by Go

--- src/main.go
+++ src/main.go
@@ -17,9 +17,26 @@ var store = sessions.NewCookieStore([]byte("password1"))
 func main() {
 	fmt.Println("Start ...")
 
+	//	client = redis.NewClient(&redis.Options{
+	//		Addr: "127.0.0.1:6379",
+	//	})
+
 	client = redis.NewClient(&redis.Options{
-		Addr: "127.0.0.1:6379",
+		Addr:     "54.67.84.81:6379",
+		Password: "",
+		DB:       0, // use default DB
 	})
+
+	/*	func NewRedisServer() *redis.Client {
+			client := redis.NewClient(&redis.Options{
+				Addr:     "13.57.96.105:6379",
+				Password: "foobared",
+				DB:       0,  // use default DB
+			})
+
+		  return client
+		}
+	*/
 
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 	router := mux.NewRouter()
