523a40de172395b7a5cb7306d6bbe6843d7aa80b
Vu Nguyen (vungu2) <vungu2@cisco.com>
2018-05-05 09:51:12 -0700

    new templates

---
+++ src/models.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"time"
+)
+
+// User represents a blog user.
+type customer struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+// Comment represents a comment a user can post in a blog post.
+type order struct {
+	ID        int       `json:"id"`
+	User      User      `json:"user"`
+	Text      string    `json:"text"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
+// Post represents a blog post.
+type Post struct {
+	ID        int       `json:"id"`
+	User      User      `json:"user"`
+	Topic     string    `json:"topic"`
+	Text      string    `json:"text"`
+	Comment   Comment   `json:"comment"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
+// Posts represents a list of Posts of a User.
+type Posts []Post
+
+// Comments represents a list of Comments in a Post.
+type Comments []Comment
+
+// Users represents a list of Users of this blog.
+type Users []User
